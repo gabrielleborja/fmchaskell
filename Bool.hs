b9165c84a65f3b0e71de847e4080975c2fab7ac3
gabrielle borja <gabrielle.borja.091@ufrn.edu.br>
2023-10-03 16:30:13 -0300

    Comentário if_then_else2

--- Bool.hs
+++ Bool.hs
@@ -5,6 +5,7 @@ import Prelude hiding (Bool, True, False, if_then_else2)
 data Bool = False | True 
     deriving ( Eq , Show)
 
+--função desenvolvida em grupo com outros alunos
 if_then_else2 :: Bool -> Bool -> Bool -> Bool
 if_then_else2 True n _ = n
 if_then_else2 False _ m = m
